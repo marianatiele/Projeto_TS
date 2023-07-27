7d935c64bd8c0a802353ab0b199c9e608bfa823a
Maria Natiele <marianatiele20@gmail.com>
2023-07-26 23:27:34 -0300

    Validações

--- Front/js/scripts.js
+++ Front/js/scripts.js
@@ -6,3 +6,34 @@ function openMenu(){
 function closeMenu(){
     document.getElementById("menu").style.width = "0";
 }
+//Validações
+function ValidaForm(){
+    
+    let nome = document.getElementById('nome')
+    let email = document.getElementById('email').value
+    
+
+    //Lógica 
+    let validaNome = nome.value != 0 && nome.value !== '' && isNaN(nome.value)
+    let resposta = validaEmail(email)
+    if(resposta == true){
+        console.log('email valido')
+    
+    }else{
+        console.log('email invalido')
+    }
+
+    //Chamando o back de acordo com a validação
+    if(validaNome && resposta == true){
+        EnviaMeg()
+    }
+
+    
+}
+//validando o email
+function validaEmail(email){
+       let re = /\S+@\S+\.\S+/;
+        return re.test(email);
+ 
+}
+   
